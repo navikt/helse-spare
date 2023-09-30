1c089651ada76c9d31a41f9874a311120b5bce33
david steinsland <david@davidsteinsland.net>
2023-09-30 17:08:49 +0200

    bumper versjoner

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,18 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val jvmTarget = "17"
 
-val junitJupiterVersion = "5.9.0"
-val testcontainersVersion = "1.17.4"
-val rapidsAndRiversVersion = "2022110411121667556720.8a951a765583"
+val junitJupiterVersion = "5.10.0"
+val testcontainersVersion = "1.19.0"
+val rapidsAndRiversVersion = "2023093008351696055717.ffdec6aede3d"
 val hikariCPVersion = "5.0.1"
-val postgresqlVersion = "42.5.0"
+val postgresqlVersion = "42.6.0"
 val flywayCoreVersion = "9.7.0"
 val kotliqueryVersion = "1.9.0"
 
 val mainClass = "no.nav.helse.spare.AppKt"
 
 plugins {
-    kotlin("jvm") version "1.7.20"
+    kotlin("jvm") version "1.9.10"
 }
 
 dependencies {
@@ -64,14 +64,13 @@ tasks {
 
         doLast {
             configurations.runtimeClasspath.get().forEach {
-                val file = File("$buildDir/libs/${it.name}")
-                if (!file.exists())
-                    it.copyTo(file)
+                val file = File("${layout.buildDirectory.get()}/libs/${it.name}")
+                if (!file.exists()) it.copyTo(file)
             }
         }
     }
 
     withType<Wrapper> {
-        gradleVersion = "7.4.2"
+        gradleVersion = "8.3"
     }
 }
