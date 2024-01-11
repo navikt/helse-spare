da518a2c0a432e536db13a4a13b70873035f38da
david steinsland <david@davidsteinsland.net>
2024-01-11 17:27:20 +0100

    java 21 og google artifact registry 🎉

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-val jvmTarget = "17"
+val jvmTarget = 21
 
 val junitJupiterVersion = "5.10.0"
 val testcontainersVersion = "1.19.0"
-val rapidsAndRiversVersion = "2023093008351696055717.ffdec6aede3d"
+val rapidsAndRiversVersion = "2024010209171704183456.6d035b91ffb4"
 val hikariCPVersion = "5.0.1"
 val postgresqlVersion = "42.6.0"
 val flywayCoreVersion = "9.7.0"
@@ -13,7 +11,7 @@ val kotliqueryVersion = "1.9.0"
 val mainClass = "no.nav.helse.spare.AppKt"
 
 plugins {
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "1.9.22"
 }
 
 dependencies {
@@ -23,28 +21,36 @@ dependencies {
     implementation("org.flywaydb:flyway-core:$flywayCoreVersion")
     implementation("com.github.seratch:kotliquery:$kotliqueryVersion")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     testImplementation("org.testcontainers:postgresql:$testcontainersVersion")
 }
 
 repositories {
+    val githubPassword: String? by project
     mavenCentral()
-    maven("https://jitpack.io")
+    /* ihht. https://github.com/navikt/utvikling/blob/main/docs/teknisk/Konsumere%20biblioteker%20fra%20Github%20Package%20Registry.md
+        så plasseres github-maven-repo (med autentisering) før nav-mirror slik at github actions kan anvende førstnevnte.
+        Det er fordi nav-mirroret kjører i Google Cloud og da ville man ellers fått unødvendige utgifter til datatrafikk mellom Google Cloud og GitHub
+     */
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/maven-release")
+        credentials {
+            username = "x-access-token"
+            password = githubPassword
+        }
+    }
+    maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(jvmTarget)
+    }
 }
 
 tasks {
-
-    named<KotlinCompile>("compileKotlin") {
-        kotlinOptions.jvmTarget = jvmTarget
-    }
-
-    named<KotlinCompile>("compileTestKotlin") {
-        kotlinOptions.jvmTarget = jvmTarget
-    }
-
     withType<Test> {
         useJUnitPlatform()
         testLogging {
@@ -71,6 +77,6 @@ tasks {
     }
 
     withType<Wrapper> {
-        gradleVersion = "8.3"
+        gradleVersion = "8.5"
     }
 }
