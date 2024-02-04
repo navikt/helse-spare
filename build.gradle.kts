f121c44e7605827e38328c1c7cd5af5df1b69c84
david steinsland <david@davidsteinsland.net>
2024-02-04 22:16:55 +0100

    bumper r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ val jvmTarget = 21
 
 val junitJupiterVersion = "5.10.0"
 val testcontainersVersion = "1.19.0"
-val rapidsAndRiversVersion = "2024020421571707080262.b74e641fd6dd"
+val rapidsAndRiversVersion = "2024020422121707081126.d834a5d05bb2"
 val hikariCPVersion = "5.0.1"
 val postgresqlVersion = "42.6.0"
 val flywayCoreVersion = "9.7.0"
