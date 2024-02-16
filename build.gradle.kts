12725e12de50db3441329ff11499ae3a1529a5e1
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2024-02-16 22:12:12 +0100

    Bump junit to 5.10.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 val jvmTarget = 21
 
-val junitJupiterVersion = "5.10.0"
+val junitJupiterVersion = "5.10.2"
 val testcontainersVersion = "1.19.0"
 val rapidsAndRiversVersion = "2024020507581707116327.1c34df474331"
 val hikariCPVersion = "5.0.1"
