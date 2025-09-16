dcf07592b7d00a7d58844fcb45b8aafe0c483cba
Amalie Mansåker <amalie.erdal.mansaker@nav.no>
2025-09-16 13:32:09 +0200

    Bytter baseimage og bumper dependencies
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ val junitJupiterVersion = "5.12.1"
 val rapidsAndRiversVersion = "2025081612341755340488.ff2c2d01e04f"
 val tbdLibsVersion = "2025.08.16-09.21-71db7cad"
 val hikariCPVersion = "6.3.0"
-val postgresqlVersion = "42.7.5"
+val postgresqlVersion = "42.7.7"
 val flywayVersion = "11.5.0"
 val kotliqueryVersion = "1.9.0"
 
